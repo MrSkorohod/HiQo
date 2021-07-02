944149273bdedea5f4a7c4fd34c01257254c043b
MrSkorohod <mr.vl.skorohod@gmail.com>
2021-07-02 09:59:12 +0300

    reorganization file,add new mixins

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 'use strict';
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
-var concat = require('gulp-concat');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
 sass.compiler = require('node-sass');
+
 
 gulp.task('sass', function () {
     return gulp.src('./stylesheets/main.scss')
